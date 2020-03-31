92346b27014f76d9b9fb537facf543fb5ec909b4
nicnys <nirivonsiri@gmail.com>
2020-03-31 09:46:16 +0200

    Add Maven project for the Plant Description Engine

---
+++ plantdescriptionengine/src/main/java/eu/arrowhead/core/plantdescriptionengine/PlantDescriptionEngineMain.java
@@ -0,0 +1,14 @@
+package eu.arrowhead.core.plantdescriptionengine;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class PlantDescriptionEngineMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
