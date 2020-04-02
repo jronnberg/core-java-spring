0ec5b775a5029749179dfa33358b73df1fa617df
nicnys <nirivonsiri@gmail.com>
2020-04-02 11:19:03 +0200

    Separate packages for PDE management and DTOs

---
+++ plantdescriptionengine/src/main/java/eu/arrowhead/core/plantdescriptionengine/dto/DtoWriter.java
@@ -0,0 +1,40 @@
+package eu.arrowhead.core.plantdescriptionengine.dto;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import se.arkalix.dto.binary.BinaryWriter;
+
+public class DtoWriter implements BinaryWriter {
+
+    private OutputStream writer;
+
+    public DtoWriter(OutputStream writer) {
+        this.writer = writer;
+    }
+
+    public int writeOffset() {
+        // TODO: Implement
+        return 0;
+    }
+
+    public void writeOffset(final int offset) {
+        // TODO: Implement
+    }
+
+    public void write(final byte b) {
+        try {
+            writer.write(b);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void write(final byte[] bytes) {
+        try {
+            writer.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ plantdescriptionengine/src/main/java/eu/arrowhead/core/plantdescriptionengine/dto/PdeSystem.java
@@ -0,0 +1,23 @@
+package eu.arrowhead.core.plantdescriptionengine.dto;
+
+import java.util.List;
+
+import se.arkalix.dto.DtoReadableAs;
+import se.arkalix.dto.DtoWritableAs;
+
+import static se.arkalix.dto.DtoEncoding.JSON;
+
+/**
+ * Data Transfer Object (DTO) interface for plant description system ports.
+ */
+@DtoReadableAs(JSON)
+@DtoWritableAs(JSON)
+public interface PdeSystem {
+
+    String systemName();
+    List<PdePort> ports();
+
+    default String asString() {
+        return "PdeSystem[systemName=" + systemName() + "]";
+    }
+}
