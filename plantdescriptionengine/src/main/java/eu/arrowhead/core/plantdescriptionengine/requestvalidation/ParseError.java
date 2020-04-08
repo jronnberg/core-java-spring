57c33af3ad0e85502b1cac87604c66b1318deed7
nicnys <nirivonsiri@gmail.com>
2020-04-08 11:12:34 +0200

    Improved PDE documentation

--- plantdescriptionengine/src/main/java/eu/arrowhead/core/plantdescriptionengine/requestvalidation/ParseError.java
+++ plantdescriptionengine/src/main/java/eu/arrowhead/core/plantdescriptionengine/requestvalidation/ParseError.java
@@ -1,5 +1,9 @@
 package eu.arrowhead.core.plantdescriptionengine.requestvalidation;
 
+/**
+ * An instance of this class represents a single query parameter requriement
+ * violation, e.g. an invalid value for a given parameter.
+ */
 class ParseError {
 
     private final String message;
