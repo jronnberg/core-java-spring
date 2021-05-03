c8464cb23f926ff4a3633d00fa1fba001abc38aa
Nicklas Nyström <nicklas.nystrom@xarepo.com>
2021-05-03 10:14:41 +0200

    Rearrange code

--- src/main/java/eu/arrowhead/core/plantdescriptionengine/PdeMain.java
+++ src/main/java/eu/arrowhead/core/plantdescriptionengine/PdeMain.java
@@ -3,7 +3,6 @@ package eu.arrowhead.core.plantdescriptionengine;
 public final class PdeMain {
 
     public static void main(final String[] args) {
-        System.out.println("DERP!");
         InventoryId id = new InventoryIdDto.Builder().id("hej").build();
         System.out.println(id);
     }
